ef1530f46e7e67eb8320334e3e0ce71df283f80a
Timothy Harley <timothy.d.harley@gmail.com>
2018-09-10 01:03:07 -0500

    wip

--- main.js
+++ main.js
@@ -6,7 +6,26 @@ const printToDom = (stringToPrint, divId) => {
 };
 
 const English = () => {
-    var translation = document.getElementById('message').value ;
+    var translation = document.getElementById('message').value;
     printToDom(translation, 'outputDiv')
 };
 
+const spanishWords = {
+    " a ":" un ",
+    " and ":" y ",
+    " christmas ":" navidad ",
+    " happy ":" contento ",
+    " holidays ":" vacaciones ",
+    " i ":" yo ",
+    " love ":" amor ",
+    " merry ":" alegre ",
+    " new ":" nuevo ",
+    " wish ":" deseo ",
+    " year ":" año ",
+    " you ":" tú "
+};
+
+const Spanish = () => {
+    var translation = document.getElementById('message').value;
+    
+}
