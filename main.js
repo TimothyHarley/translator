d6cd0f4660cba62dd485802f53f81d810fd049bb
Timothy Harley <timothy.d.harley@gmail.com>
2018-09-09 23:30:04 -0500

    wrote code to print the English text to dom

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log ('test');
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+};
+
+const English = () => {
+    var translation = document.getElementById('message').value ;
+    printToDom(translation, 'outputDiv')
+};
+
